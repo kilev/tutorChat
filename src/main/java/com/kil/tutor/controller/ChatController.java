c6a641dde845bfb0e8311de54bddedff406a67b7
kilev <kooler.kilev@gmail.com>
2020-11-13 00:33:24 +0700

    tmp

--- src/main/java/com/kil/tutor/controller/ChatController.java
+++ src/main/java/com/kil/tutor/controller/ChatController.java
@@ -4,8 +4,6 @@ import com.kil.tutor.entity.user.User;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -13,7 +11,7 @@ public class ChatController {
 
     @GetMapping("test")
     public ResponseEntity<String> test(@AuthenticationPrincipal User user) {
-        return ResponseEntity.ok().body("test passed");
+        return ResponseEntity.ok().body("test passed successful!");
     }
 
 //    @GetMapping
